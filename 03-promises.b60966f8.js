0cc01bd263bad069a29c3d7d904b4a4237da77ca
InnaKorniienko <97888218+InnaKorniienko@users.noreply.github.com>
2022-08-11 13:03:20 +0000

    Deploying to gh-pages from @ InnaKorniienko/goit-js-hw-09@23e35a88e272d9b474e3dfc01d17c6cf92c261a9 🚀

--- 03-promises.b60966f8.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".form");function e(o,e){Math.random();setTimeout((()=>{}),e)}console.log(o),o.addEventListener("submit",e),e().then((({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)}));
-//# sourceMappingURL=03-promises.b60966f8.js.map
